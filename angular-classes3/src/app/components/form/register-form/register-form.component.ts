a7e138f1969c03915a7c85ccbd845d70775c67fd
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2024-01-19 16:11:53 -0300

    form group

--- angular-classes3/src/app/components/form/register-form/register-form.component.ts
+++ angular-classes3/src/app/components/form/register-form/register-form.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register-form.component.html',
   styleUrl: './register-form.component.css'
 })
 export class RegisterFormComponent {
+  registerForm = new FormGroup({
+    name: new FormControl(""),
+    email: new FormControl(""),
+    password: new FormControl("")
+  });
 
+  onSubmit(){
+    console.log(this.registerForm.value);
+    this.registerForm.reset();
+  }
 }
