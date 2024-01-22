227795b61314c89435fbf950911769e9591d6ef8
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2024-01-22 09:44:58 -0300

    rendering errors

--- angular-classes3/src/app/components/form/register-form/register-form.component.ts
+++ angular-classes3/src/app/components/form/register-form/register-form.component.ts
@@ -1,21 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './register-form.component.html',
-  styleUrl: './register-form.component.css'
+  styleUrl: './register-form.component.css',
 })
 export class RegisterFormComponent {
   registerForm = new FormGroup({
-    name: new FormControl(null, [Validators.required, Validators.min(3)]),
+    name: new FormControl(null, [Validators.required, Validators.minLength(3)]),
     email: new FormControl(null, [Validators.required, Validators.email]),
-    password: new FormControl(null, [Validators.required, Validators.min(8)])
+    password: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
   });
 
-  onSubmit(){
+  onSubmit() {
+    console.log(this.registerForm.get('password')?.errors);
     console.log(this.registerForm.status);
     this.registerForm.reset();
   }
